c85a366eea1b34d109f48eebd04b1f4df4a75e3d
dessa995 <dessa995@outlook.com>
2023-09-14 15:52:32 +0200

    use state covered

--- react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("Mom, are the predators nearby?");
+
+  const handleClick = () => {
+    if (text === "Mom, are the predators nearby?") {
+      setText("My speed potato, we are the predators.");
+    } else if (text === "My speed potato, we are the predators.") {
+      setText("We are?! :O");
+    } else {
+      setText("Mom, are the predators nearby?");
+    }
+  };
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
